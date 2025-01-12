b11821bebe3b096f6016b8e2fb23b644a8b74825
Bruno Barreiras <brunooliver368@gmail.com>
2025-01-12 00:32:10 -0300

    chore: adjust connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ const password = process.env.DB_PASSWORD;
 export async function connection() {
   try {
     return await mongoose.connect(
-      `mongodb+srv://${user}:${password}@cluster0.s6fgu.mongodb.net/`
+      `mongodb+srv://${user}:${password}@cluster0.pkews.mongodb.net/`
     );
   } catch (error) {
     console.error(error);
