917379dc7736072b585f052877fe31af07e243e8
Bruno Barreiras <brunooliver368@gmail.com>
2025-01-12 13:27:51 -0300

    feat: add photo validation

---
+++ src/middlewares/photoValidation.ts
@@ -0,0 +1,23 @@
+import { body } from "express-validator";
+
+export function photoValidation() {
+  return [
+    body("title")
+      .notEmpty()
+      .withMessage("Title cannot be empty")
+      .isString()
+      .withMessage("Title must be a string")
+      .isLength({ min: 3 })
+      .withMessage("Title must be at least 3 characters long"),
+    body("image").custom((value, { req }) => {
+      if (!req.file) {
+        throw new Error("Image file is required");
+      }
+      const fileExtension = req.file.mimetype.split("/")[1];
+      if (fileExtension !== "jpeg" && fileExtension !== "png") {
+        throw new Error("Image must be a jpg or png file");
+      }
+      return true;
+    }),
+  ];
+}
