f352cabbf41f40246e48052a77147069f4e949b2
Bruno Barreiras <brunooliver368@gmail.com>
2025-01-12 12:37:59 -0300

    feat: add update profile controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,7 +3,8 @@ import User from "../models/User";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { Types } from "mongoose";
+import mongoose, { Types } from "mongoose";
+import { hashPassword } from "../utils/hashPassword";
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -28,8 +29,7 @@ export async function register(req: Request, res: Response): Promise<void> {
       return;
     }
 
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+    const passwordHash = await hashPassword(password);
 
     const newUser = await User.create({
       name,
@@ -86,4 +86,34 @@ export async function getCurrentUser(
   const user = req.user;
 
   res.status(200).json(user);
+}
+
+export async function update(req: Request, res: Response): Promise<void> {
+  const { name, password, bio } = req.body;
+  const profileImage = req.file ? req.file.filename : null;
+  const reqUser = req.user;
+
+  try {
+    const user = await User.findById(
+      new mongoose.Types.ObjectId(reqUser)
+    ).select("-password");
+
+    if (!user) {
+      res.status(404).json({ errors: ["User not found."] });
+      return;
+    }
+
+    const updates: { [key: string]: any } = {};
+    if (name) updates.name = name;
+    if (password) updates.password = await hashPassword(password);
+    if (bio) updates.bio = bio;
+    if (profileImage) updates.profileImage = profileImage;
+
+    Object.assign(user, updates);
+    await user.save();
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ errors: ["An error occurred during the update."] });
+  }
 }
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,16 +1,31 @@
 import express from "express";
-import { getCurrentUser, login, register } from "../controllers/UserController";
+import {
+  getCurrentUser,
+  login,
+  register,
+  update,
+} from "../controllers/UserController";
 import { validate } from "../middlewares/handleValidation";
 import {
   userCreateValidation,
   userLoginValidation,
+  userUpdateValidation,
 } from "../middlewares/userValidations";
 import { authGuard } from "../middlewares/authGuard";
+import { imageUpload } from "../middlewares/imageUpload";
 
 const router = express.Router();
 
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", userLoginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/update-profile",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 
 export default router;
