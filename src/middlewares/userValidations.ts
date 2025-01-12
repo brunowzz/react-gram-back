aa10f98dfe59eb1a29af34588cafa5a525ca1f23
Bruno Barreiras <brunooliver368@gmail.com>
2025-01-12 12:37:06 -0300

    feat: add user update validation

--- src/middlewares/userValidations.ts
+++ src/middlewares/userValidations.ts
@@ -6,7 +6,9 @@ export function userCreateValidation() {
       .exists()
       .withMessage("Name is required.")
       .isString()
-      .withMessage("Name must be a string."),
+      .withMessage("Name must be a string.")
+      .isLength({ min: 3 })
+      .withMessage("Name must be at least 3 characters long."),
     body("email")
       .exists()
       .withMessage("Email is required.")
@@ -37,5 +39,26 @@ export function userLoginValidation() {
       .withMessage("Password is required.")
       .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters long."),
+  ];
+}
+
+export function userUpdateValidation() {
+  return [
+    body("name")
+      .optional()
+      .isString()
+      .withMessage("Name must be a string.")
+      .isLength({ min: 3 })
+      .withMessage("Name must be at least 3 characters long."),
+    body("email").optional().isEmail().withMessage("Email must be valid."),
+    body("password")
+      .optional()
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long."),
+    body("profileImage")
+      .optional()
+      .isString()
+      .withMessage("Profile image must be a string."),
+    body("bio").optional().isString().withMessage("Bio must be a string."),
   ];
 }
